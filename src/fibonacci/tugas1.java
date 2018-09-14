47da81f8c0cb04020635f5633ed68981bed53aff
Anatasya <Anatasya@Anatasya>
2018-09-15 05:54:27 +0700

    Initial Commit

---
+++ src/fibonacci/tugas1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonacci;
+
+import java.util.Scanner;
+ /*
+ * @author Anatasya
+ */
+public class tugas1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Nama : Anatasya Galuh Risamita Dewi Salsadila");
+        System.out.println("No. absen : 05");
+        System.out.println("Kelas : X RPL 2");
+        System.out.println("----------------------------------------------");
+        
+    
+}
+}
+
