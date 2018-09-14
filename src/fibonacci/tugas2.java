a3a155066d5cee00bf257d68983545387815f84b
Anatasya <Anatasya@Anatasya>
2018-09-15 06:01:17 +0700

    Tambah deklarasi

--- src/fibonacci/tugas2.java
+++ src/fibonacci/tugas2.java
@@ -17,6 +17,12 @@ public class tugas2 {
         System.out.println("Kelas : X RPL 2");
         System.out.println("----------------------------------------------");
         
+        int[] berat =  {7, 5, 4, 3, 2};
+        int[] bayaran = {10000, 7000, 5000, 3000, 2000};
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Jumlah berat : ");
+        int total = scan.nextInt();
+        int bayar = 0;
         
     } 
 }
